169f98d5a69716b923cd735a51001fccc86d8dde
zack zheng <zack_zhengxiyun@163.com>
2025-07-22 16:30:53 +0800

    wip: eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,5 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
-  type: 'lib',
   ignores: ['bin/', '**/bin/**/', 'node_modules/', '**/node_modules/**/', 'dist/', '**/dist/**/'],
 })
