ece1f6cbd18fa2d3c333de854cd509a6e44b72e2
zack zheng <zack_zhengxiyun@163.com>
2025-08-11 15:11:07 +0800

    wip: remove comment code

--- src/components/ecommerce/StatisticsChart.tsx
+++ src/components/ecommerce/StatisticsChart.tsx
@@ -3,115 +3,6 @@ import { Area, AreaChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YA
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs'
 
 export default function StatisticsChart(): React.ReactElement {
-  // const options: ApexOptions = {
-  //   legend: {
-  //     show: false, // Hide legend
-  //     position: 'top',
-  //     horizontalAlign: 'left',
-  //   },
-  //   colors: ['#465FFF', '#9CB9FF'], // Define line colors
-  //   chart: {
-  //     fontFamily: 'Outfit, sans-serif',
-  //     height: 310,
-  //     type: 'line', // Set the chart type to 'line'
-  //     toolbar: {
-  //       show: false, // Hide chart toolbar
-  //     },
-  //   },
-  //   stroke: {
-  //     curve: 'straight', // Define the line style (straight, smooth, or step)
-  //     width: [2, 2], // Line width for each dataset
-  //   },
-
-  //   fill: {
-  //     type: 'gradient',
-  //     gradient: {
-  //       opacityFrom: 0.55,
-  //       opacityTo: 0,
-  //     },
-  //   },
-  //   markers: {
-  //     size: 0, // Size of the marker points
-  //     strokeColors: '#fff', // Marker border color
-  //     strokeWidth: 2,
-  //     hover: {
-  //       size: 6, // Marker size on hover
-  //     },
-  //   },
-  //   grid: {
-  //     xaxis: {
-  //       lines: {
-  //         show: false, // Hide grid lines on x-axis
-  //       },
-  //     },
-  //     yaxis: {
-  //       lines: {
-  //         show: true, // Show grid lines on y-axis
-  //       },
-  //     },
-  //   },
-  //   dataLabels: {
-  //     enabled: false, // Disable data labels
-  //   },
-  //   tooltip: {
-  //     enabled: true, // Enable tooltip
-  //     x: {
-  //       format: 'dd MMM yyyy', // Format for x-axis tooltip
-  //     },
-  //   },
-  //   xaxis: {
-  //     type: 'category', // Category-based x-axis
-  //     categories: [
-  //       'Jan',
-  //       'Feb',
-  //       'Mar',
-  //       'Apr',
-  //       'May',
-  //       'Jun',
-  //       'Jul',
-  //       'Aug',
-  //       'Sep',
-  //       'Oct',
-  //       'Nov',
-  //       'Dec',
-  //     ],
-  //     axisBorder: {
-  //       show: false, // Hide x-axis border
-  //     },
-  //     axisTicks: {
-  //       show: false, // Hide x-axis ticks
-  //     },
-  //     tooltip: {
-  //       enabled: false, // Disable tooltip for x-axis points
-  //     },
-  //   },
-  //   yaxis: {
-  //     labels: {
-  //       style: {
-  //         fontSize: '12px', // Adjust font size for y-axis labels
-  //         colors: ['#6B7280'], // Color of the labels
-  //       },
-  //     },
-  //     title: {
-  //       text: '', // Remove y-axis title
-  //       style: {
-  //         fontSize: '0px',
-  //       },
-  //     },
-  //   },
-  // }
-
-  // const series = [
-  //   {
-  //     name: 'Sales',
-  //     data: [180, 190, 170, 160, 175, 165, 170, 205, 230, 210, 240, 235],
-  //   },
-  //   {
-  //     name: 'Revenue',
-  //     data: [40, 30, 50, 40, 55, 40, 70, 100, 110, 120, 150, 140],
-  //   },
-  // ]
-
   const data = [
     {
       name: 'Jan',
@@ -231,8 +122,6 @@ export default function StatisticsChart(): React.ReactElement {
               <Area type="linear" dataKey="revenue" stackId="1" stroke="#a0a0ff" fill="#a0a0ff" />
             </AreaChart>
           </ResponsiveContainer>
-
-          {/* <Chart options={options} series={series} type="area" height={310} /> */}
         </div>
       </div>
     </div>
