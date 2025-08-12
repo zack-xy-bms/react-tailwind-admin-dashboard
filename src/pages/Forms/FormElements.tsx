a37187e72231d216fc98976dad60f189277d8e76
zack zheng <zack_zhengxiyun@163.com>
2025-08-12 17:57:20 +0800

    wip: try form

--- src/pages/Forms/FormElements.tsx
+++ src/pages/Forms/FormElements.tsx
@@ -1,7 +1,55 @@
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { Button } from '@/components/ui/button'
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: 'Username must be at least 2 characters.',
+  }).max(10, {
+    message: 'Username must less than 10 characters.',
+  }),
+})
+
 export default function FormElements() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: '',
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // eslint-disable-next-line no-console
+    console.log(values)
+  }
+
   return (
-    <div>
-      Form元素
-    </div>
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormMessage />
+                <FormDescription>This is your public display name.</FormDescription>
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+
+    </>
   )
 }
